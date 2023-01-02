d064e8e67ca0cd1c937f6b12832140c756b1278a
unknown <pbjavouam@gmail.com>
2023-01-02 10:39:42 -0600

    Se corrigen issues

--- src/main/java/jpa/example/demo/entity/Cuenta.java
+++ src/main/java/jpa/example/demo/entity/Cuenta.java
@@ -9,6 +9,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity(name = "cuentas")
 public class Cuenta {
 
