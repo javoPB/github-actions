ffa7886917036ed5b26022eede12c9e4684244e3
javoPB <javier.perezb@softtek.com>
2023-01-10 19:40:31 -0600

    Otros cambios

--- src/main/java/jpa/example/demo/DemoApplication.java
+++ src/main/java/jpa/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	//Método main - 
+	//Método main - Otro comentario
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
